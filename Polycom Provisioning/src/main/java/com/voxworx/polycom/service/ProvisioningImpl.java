3bab83d81fcbe4ff73ce4ba582374cc7c70b37eb
imcmaster <ian.mcmaster@gmail.com>
2014-02-07 17:55:09 -0500

    Second test to push to github

---
+++ Polycom Provisioning/src/main/java/com/voxworx/polycom/service/ProvisioningImpl.java
@@ -0,0 +1,11 @@
+package com.voxworx.polycom.service;
+
+public class ProvisioningImpl implements ProvisioningService {
+
+	@Override
+	public void installMasterFile(String mac) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
