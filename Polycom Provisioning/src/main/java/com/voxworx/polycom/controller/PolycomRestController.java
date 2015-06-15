1ca69b9f51228eb2348e0ead4694305781912f65
imcmaster <ian.mcmaster@gmail.com>
2015-06-15 12:55:44 -0400

    Adding PUT support for phone update

--- Polycom Provisioning/src/main/java/com/voxworx/polycom/controller/PolycomRestController.java
+++ Polycom Provisioning/src/main/java/com/voxworx/polycom/controller/PolycomRestController.java
@@ -1,15 +1,23 @@
 package com.voxworx.polycom.controller;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.voxworx.polycom.dao.PhoneDAO;
 import com.voxworx.polycom.domain.SipPhone;
 
@@ -37,5 +45,27 @@ public class PolycomRestController {
 		logger.info("REST GET request for phone id="+phoneId);
 		return phoneDAO.findById(phoneId);
 	}
+	
+	@RequestMapping(value="/polycom/phone/{phoneId}", method = RequestMethod.PUT)
+	public ResponseEntity<?> updateSipPhone(@PathVariable int phoneId, HttpServletRequest req) {
+		logger.info("REST PUT request for phone id="+phoneId);
+		ObjectMapper mapper = new ObjectMapper();
+		String body = null;
+		try {
+			body = req.getReader().readLine();
+			SipPhone phone = mapper.readValue(body, SipPhone.class);
+			logger.info("got phone="+phone);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//TODO:  USE DAO to update the record
+		// Response code DEPENDS on success of DAO action!
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("test"));
+        return new ResponseEntity<String>(null, headers, HttpStatus.CREATED);
+
+	}
+	
 	 
 }
