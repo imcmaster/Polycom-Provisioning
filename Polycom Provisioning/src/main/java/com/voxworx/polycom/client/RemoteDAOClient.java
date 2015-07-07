88544b8339b61e139dc44f424f91de91e89b37ad
imcmaster <ian.mcmaster@gmail.com>
2015-07-06 21:19:42 -0400

    Added packagesToScan

--- Polycom Provisioning/src/main/java/com/voxworx/polycom/client/RemoteDAOClient.java
+++ Polycom Provisioning/src/main/java/com/voxworx/polycom/client/RemoteDAOClient.java
@@ -1,14 +1,17 @@
 package com.voxworx.polycom.client;
 
+import java.util.List;
+
 import com.voxworx.polycom.dao.PhoneDAO;
+import com.voxworx.polycom.domain.NatParameters;
 import com.voxworx.polycom.domain.SipPhone;
 import com.voxworx.utils.MockUtils;
 import com.voxworx.utils.SpringRemotingClient;
 
 public class RemoteDAOClient {
 
-	//private static final String host = "192.168.2.7";
-	private static final String host = "67.213.75.234";
+	private static final String host = "192.168.2.7";
+	//private static final String host = "67.213.75.234";
 
 	public static void main(String[] args) {
 		new RemoteDAOClient().go();
@@ -17,6 +20,11 @@ public class RemoteDAOClient {
 	private void go() {
 		
 		PhoneDAO client = SpringRemotingClient.getPolycomPhoneDAORemotingClient(host);
+		
+		List<NatParameters> nps = client.findAllNatParameters();
+		System.out.println("Size="+nps.size());
+		System.exit(0);
+		
 		//client.addPhone(MockUtils.createSipPhone());
 		for (SipPhone p : client.findAll())
 			System.out.println("id="+p.getUserId());
